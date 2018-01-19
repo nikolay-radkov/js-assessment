74110f27cc07bb5bec3eedbd7cc54c14c49fde01
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2018-01-19 22:08:35 +0200

    Implement async functions

--- app/async.js
+++ app/async.js
@@ -7,7 +7,7 @@ asyncAnswers = {
    * @returns {then: function} A promise like object containing a then property.
    */
   async: function async(value) {
-
+    return Promise.resolve(value);
   },
 
   /**
@@ -21,6 +21,10 @@ asyncAnswers = {
    * @returns {then: function} A promise like object containing a then property.
    */
   manipulateRemoteData: function manipulateRemoteData(url) {
-
+    return new Promise((resolve) => {
+      $.get(url, (result) => {
+        resolve(result.people.map(m => m.name).sort());
+      });
+    });
   },
 };
